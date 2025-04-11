97c8a2fe6e88151aafe2df573c566c53d6a9010f
mikodomi <mikodominski@gmail.com>
2025-04-11 22:54:30 +0200

    scytale encryption

---
+++ scytale/scytale.c
@@ -0,0 +1,56 @@
+#include "../misc.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char* encrypt(const char* text, int key) {
+    const int size = strlen(text);
+    char* result = malloc(size+1);
+    if (!result) return NULL;
+
+    int res_ind = 0;
+    for (int i = 0; i<key; i++) {
+        int text_ind = i;
+        while (text_ind < size && res_ind < size) {
+            result[res_ind] = text[text_ind];
+            text_ind += key;
+            res_ind++;
+        }
+    }
+
+    result[size] = '\0';
+    return result;
+}
+
+// very slow and memory inefficient - use wisely
+void visual_rep(const char* text, int key) {
+    const int size = strlen(text);
+    const int temp_size = ((size+1)*key); // size+1 for \n on every line and \0 on last line
+    char* temp = malloc(temp_size); 
+    if (!temp) return;
+    for (int i = 0; i<temp_size; i++) {
+        if (i > 0 && (i+1)%(size+1) == 0) {
+            temp[i] = '\n';
+            continue;
+        }
+        temp[i] = '.';
+    }
+
+    for (int i = 0; i<size; i++) {
+        temp[(i+(i*(size+1)))%temp_size] = text[i];
+    }
+    temp[temp_size-1] = '\0';
+    printf("%s\n", temp);
+    free(temp);
+}
+
+int main() {
+    visual_rep("NIECHSIEDZIEJEWOLANIEBA", 3);
+    char* result = encrypt("NIECHSIEDZIEJEWOLANIEBA", 3);
+
+    printf("%s\n", result);
+
+    free(result);
+    return 0;
+}
+
